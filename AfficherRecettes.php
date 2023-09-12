5ba97de6315d5395b6d4d9373ce0149f94a38be5
ebShare <127939817+ebShare@users.noreply.github.com>
2023-09-12 08:27:31 +0200

    Les tableaux associatifs

--- AfficherRecettes.php
+++ AfficherRecettes.php
@@ -25,11 +25,14 @@
         <title>Affichage des recettes
         </title>
     </head>
-    <body>
+    <body style="text-align:center">
+        <h1>Affichage des recettes</h1>
         <ul>
             <?php 
                 foreach($recipes as $recipe) {
-                        echo $recipe['title'].' auteur : '. $recipe['author'].'<br>';
+                    if ($recipe['is_enabled']==true){
+                        echo '<h2>'.$recipe['title'].'</h2>'.' auteur : '. $recipe['author'].'<br><br>';
+                    }
                 }
             ?>
         </ul>
