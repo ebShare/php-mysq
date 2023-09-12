770a057cc754e18b47259eede96d8415dbaf3717
ebShare <127939817+ebShare@users.noreply.github.com>
2023-09-12 17:12:52 +0200

    Sécurisez votre code

--- submit_contact.php
+++ submit_contact.php
@@ -35,11 +35,11 @@
                 <h5 class="card-title">Rappel de vos informations</h5>
                 <p class="card-text"><b>Email</b>:
 
-                    <?php echo $_POST['email']; ?>
+                    <?php echo htmlspecialchars($_POST['email']); ?>
                 </p>
                 <p class="card-text">
                     <b>Message</b>:
-                    <?php echo $_POST['message']; ?>
+                    <?php echo htmlspecialchars($_POST['message']); ?>
                 </p>
             </div>
         </div>
