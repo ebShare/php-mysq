30502c5be517e55ccac47cb8ef1b4b1ee7b8c073
ebShare <127939817+ebShare@users.noreply.github.com>
2023-09-13 10:36:03 +0200

    Ajout d'une clé password dans le tableau  de variables.php

--- variables.php
+++ variables.php
@@ -6,16 +6,19 @@ $users = [
         'full_name' => 'Mickaël Andrieu',
         'email' => 'mickael.andrieu@exemple.com',
         'age' => 34,
+        'password'=> 'MA34',
     ],
     [
         'full_name' => 'Mathieu Nebra',
         'email' => 'mathieu.nebra@exemple.com',
         'age' => 34,
+        'password'=> 'MN34',
     ],
     [
         'full_name' => 'Laurène Castor',
         'email' => 'laurene.castor@exemple.com',
         'age' => 28,
+        'password'=> 'LC28',
     ],
 ];
 
