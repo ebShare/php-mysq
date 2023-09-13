680604cf1273a07dc2b8d633589919244d5f3e00
ebShare <127939817+ebShare@users.noreply.github.com>
2023-09-13 11:39:49 +0200

    Implémentez un système de connexion

--- index.php
+++ index.php
@@ -24,15 +24,20 @@
         ?>
 
         <!-- inclusion de l'entête du site -->
+
         <?php include_once('header.php'); ?>
+
+        <?php include_once('login.php');?>
         
-        <?php foreach(getRecipes($recipes) as $recipe) : ?>
-            <article>
-                <h3><?php echo $recipe['title']; ?></h3>
-                <div><?php echo $recipe['recipe']; ?></div>
-                <i><?php echo displayAuthor($recipe['author'], $users); ?></i>
-            </article>
-        <?php endforeach ?>
+        <?php if(isset($loggedUser)):?>
+            <?php foreach(getRecipes($recipes) as $recipe) : ?>
+                <article>
+                    <h3><?php echo $recipe['title']; ?></h3>
+                    <div><?php echo $recipe['recipe']; ?></div>
+                    <i><?php echo displayAuthor($recipe['author'], $users); ?></i>
+                </article>
+            <?php endforeach ?>
+        <?php endif;?>
     </div>
 
     <!-- inclusion du bas de page du site -->
